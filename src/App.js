3ebc391567b0f6c1d288f9d2eeb995cd966da385
yoojj1990 <yoojj1990@gmau>
2022-07-05 13:17:13 +0900

    리액트 배열 연습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
+import { useRef, useState } from "react";
+
 import './App.css';
 
+
+
+function User({user}) {
+  return (
+    <div>
+      <h1>이름 : {user.username}</h1>
+      <h1>이메일 : {user.email}</h1>
+    </div>
+  );
+}
+
+function CreateUser({username, email, onChange, onCreate}) {
+  return (
+    <div>
+      이름 : <input name="username" onChange={onChange} value={username}></input><br></br>
+      이메일 : <input name="email" onChange={onChange} value={email}></input>
+      <button onClick={onCreate}>등록</button>
+    </div>
+  );
+}
+
+function UserList({users}) {
+  // const users = [
+  //   {id: 1, username:"홍길동", email:"hong@abc.com"}
+  //   {id: 2, username:"이순신", email:"lee@abc.com"}
+  // ]
+  return (
+    <div>
+      {users.map(user => (
+        <User user={user} key={user.id}></User>
+      ))}
+    </div>
+  );
+}
+
 function App() {
+  const [inputs, setInputs] = useState({
+    username:"",
+    email:""
+  });
+  const {username, email} = inputs;
+
+  const onChange = (e) => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]:value
+    });
+  };
+
+  const [users, setUsers] = useState([
+    {id:1, username:"홍길동", email:"hong@abc.com"},
+    {id:2, username:"이순신", email:"lee@abc.com"}
+  ]);
+
+  const nextId = useRef(3);
+  const onCreate = () => {
+    const user = {
+      id : nextId.current,
+      username,
+      email
+    };
+    setUsers([...users, user]);
+
+    setInputs({
+      username:"",
+      email:""
+    });
+    nextId.current += 1;
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CreateUser 
+      username={username}
+      email={email}
+      onCreate={onCreate}
+      onChange={onChange}
+      ></CreateUser>
+      <UserList users={users} />
     </div>
   );
 }
