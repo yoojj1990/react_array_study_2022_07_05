5c725dc444d131973ba705c2f4b280b3c5f7ee4e
yoojj1990 <yoojj1990@gmau>
2022-07-05 14:12:44 +0900

    배열 원소 추가, 삭제 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
-import { useRef, useState } from "react";
-
+import { useRef, useState } from 'react';
 import './App.css';
 
-
-
-function User({user}) {
-  return (
+function User({user, onRemove, onToggle}) {
+  return(
     <div>
-      <h1>이름 : {user.username}</h1>
+      <h1>이름 :         
+        <b
+          style={{
+            cursor: "pointer",
+            color:user.active ? "red":"blue"
+          }}
+          onClick={()=> onToggle(user.id)}
+        >
+        {user.username}
+        </b>
+      </h1>
       <h1>이메일 : {user.email}</h1>
+      <button onClick={()=>onRemove(user.id)}>삭제</button>
     </div>
   );
 }
 
 function CreateUser({username, email, onChange, onCreate}) {
-  return (
+  return(
     <div>
-      이름 : <input name="username" onChange={onChange} value={username}></input><br></br>
-      이메일 : <input name="email" onChange={onChange} value={email}></input>
+      이름 : <input name='username' onChange={onChange} value={username}></input><br></br>
+      이메일 : <input name='email' onChange={onChange} value={email}></input>
       <button onClick={onCreate}>등록</button>
     </div>
   );
 }
 
-function UserList({users}) {
+function UserList({users, onRemove, onToggle}) {
   // const users = [
-  //   {id: 1, username:"홍길동", email:"hong@abc.com"}
-  //   {id: 2, username:"이순신", email:"lee@abc.com"}
+  //   {id:1, username:"홍길동", email:"hong@abc.com"},
+  //   {id:2, username:"이순신", email:"lee@abc.com"}
   // ]
-  return (
+
+  return(
     <div>
       {users.map(user => (
-        <User user={user} key={user.id}></User>
+        <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}></User>
       ))}
     </div>
   );
@@ -44,7 +53,7 @@ function App() {
   });
   const {username, email} = inputs;
 
-  const onChange = (e) => {
+  const onChange = (e)=> {
     const {name, value} = e.target;
     setInputs({
       ...inputs,
@@ -53,19 +62,20 @@ function App() {
   };
 
   const [users, setUsers] = useState([
-    {id:1, username:"홍길동", email:"hong@abc.com"},
-    {id:2, username:"이순신", email:"lee@abc.com"}
+    {id:1, username:"홍길동", email:"hong@abc.com", active: false},
+    {id:2, username:"이순신", email:"lee@abc.com", active: true}
   ]);
 
   const nextId = useRef(3);
-  const onCreate = () => {
+  const onCreate = ()=> {
+    console.log('idnum',nextId);
     const user = {
       id : nextId.current,
       username,
       email
     };
     setUsers([...users, user]);
-
+    console.log('user',user);
     setInputs({
       username:"",
       email:""
@@ -73,16 +83,27 @@ function App() {
     nextId.current += 1;
   };
 
+  const onRemove = (id)=> {
+    setUsers(users.filter(user => user.id !== id));
+  };
+
+  const onToggle = (id)=> {
+    setUsers(
+      users.map(user=>
+        user.id === id ? {...user, active:!user.active}:user
+      )
+    );
+  };
 
   return (
     <div className="App">
       <CreateUser 
       username={username}
       email={email}
-      onCreate={onCreate}
       onChange={onChange}
+      onCreate={onCreate}
       ></CreateUser>
-      <UserList users={users} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </div>
   );
 }
